57eacf93268c0b9dcf327cc9c9a6b965d99ec59f
Jim Pryor <dubiousjim@gmail.com>
2012-06-27 10:49:06 -0400

    fast-export: fix fast-export.sh lookup

--- src/fast-export.sh
+++ src/fast-export.sh
@@ -47,15 +47,15 @@ git-current-branch () {
 }
 
 check-hg-fast-export () {
+    local home
     # Search for hg-fast-export $PATH, use if available, if not fall back
-    # to looking around for it in sibling directory of bin directory of
-    # the current executable, possibly tracing back along symbolic link.
+    # to looking around for it relative to the current executable's realpath.
     if type hg-fast-export >/dev/null 2>&1; then
 	HG_FAST_EXPORT=hg-fast-export
     else
-	GITHG_HOME=$(dirname "$($canon "$0")")
-        GITHG_HOME=$($canon "$GITHG_HOME/..")
-	HG_FAST_EXPORT=$GITHG_HOME/fast-export/hg-fast-export.sh
+	home=$(dirname "$($canon "$0")")
+        # home=$($canon "$home/../fast-export")
+	HG_FAST_EXPORT=$home/hg-fast-export.sh
 	if ! type "$HG_FAST_EXPORT" >/dev/null 2>&1; then
 	    echo "error: executable not found, $HG_FAST_EXPORT"
 	    echo 'Possible fixes: run "git submodule update --init" in git-hg repo, or'
